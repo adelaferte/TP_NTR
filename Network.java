df22c89a1f1b2e488800e7cbb98876bae6cc92bf
Jimmy Rogala <jrogala@dy172005.univ-rennes1.fr>
2017-02-14 12:03:14 +0100

    corrected small mistakes

--- Network.java
+++ Network.java
@@ -9,28 +9,28 @@ public class Network {
 			for (i=0;i<n;i = i+2){
 				for (j=0;j<n;j = j+2){
 					if (i+1 < n){
-						num_link = new Link(grid[i][j],grid[i+1][j],debmin,debmax)
+						num_link = new Link(grid[i][j],grid[i+1][j],debmin,debmax);
 						grid[i][j].addLink(num_link);
 						grid[i+1][j].addLink(num_link);
 					}
 					if (i-1 >=0){
-						num_link = new Link(grid[i][j],grid[i-1][j],debmin,debmax)
+						num_link = new Link(grid[i][j],grid[i-1][j],debmin,debmax);
 						grid[i][j].addLink(num_link);
 						grid[i-1][j].addLink(num_link);
 					}
 					if (j+1 < n){
-						num_link = new Link(grid[i][j],grid[i][j+1],debmin,debmax)
+						num_link = new Link(grid[i][j],grid[i][j+1],debmin,debmax);
 						grid[i][j].addLink(num_link);
 						grid[i][j+1].addLink(num_link);
 					}
 					if (j-1 >= 0){
-						num_link = new Link(grid[i][j],grid[i][j-1],debmin,debmax)
+						num_link = new Link(grid[i][j],grid[i][j-1],debmin,debmax);
 						grid[i][j].addLink(num_link);
 						grid[i][j-1].addLink(num_link);
 					}
 				}
 			}
-			System.out.println("Network created.")
+			System.out.println("Network created.");
 		}
 
 }
