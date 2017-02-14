f99b055a9613105ac4c5bb3107fbcbdd3c63b2d3
adelaferte <apollinedelaferte@gmail.com>
2017-02-14 11:58:05 +0100

    Link

--- Link.java
+++ Link.java
@@ -1,9 +1,11 @@
+
 public class Link {
 	
 	private Device A;
 	private Device B;
 	private int debitMin;
 	private int debitMax;
+	private int debitMoy;
 	private int debitInst;
 	
 	/**
@@ -14,45 +16,53 @@ public class Link {
 	 * 
 	 * initialise les débits
 	 */
-	Link(Device d1, Device d2){
+	Link(Device d1, Device d2, int debMax, int debMin){
 		A = d1;
 		B = d2;
-		debitMax = 20;
-		debitMin = 10;
-		debitInst = 0;
+		debitMax = debMax;
+		debitMin = debMin;
+		setDebitMoy((debitMax+debitMin)/2);
+		randomDebitInstant();
+	}
+	
+	Link(int debMax, int debMin){
+		A = null;
+		B = null;
+		debitMax = debMax;
+		debitMin = debMin;
+		setDebitMoy((debitMax+debitMin)/2);
+	}
+	
+	/**
+	 * return le voisin du Device a
+	 * @param a Device
+	 * @return le voisin du Device a
+	 */
+	public Device getVoisin(Device a){
+		
+		if(A == a)
+			return B;
+		else
+			return A;
 	}
 
 	public Device getA() {
 		return A;
 	}
 
-	public void setA(Device a) {
-		A = a;
-	}
-
 	public Device getB() {
 		return B;
 	}
 
-	public void setB(Device b) {
-		B = b;
-	}
 
 	public int getDebitMin() {
 		return debitMin;
-	}
-
-	public void setDebitMin(int debitMin) {
-		this.debitMin = debitMin;
 	}
 
 	public int getDebitMax() {
 		return debitMax;
 	}
 
-	public void setDebitMax(int debitMax) {
-		this.debitMax = debitMax;
-	}
 
 	public int getDebitInst() {
 		return debitInst;
@@ -62,10 +72,22 @@ public class Link {
 		this.debitInst = debitInst;
 	}
 	
-	
-	
-	
-	
-	
+	/**
+	 * initialisation du débit instantané
+	 * @return le debitInst avec sa nouvelle valeur.
+	 */
+	public int randomDebitInstant(){
+		int random = getDebitMin() + (int)(Math.random() * ((getDebitMax() - getDebitMin()) + 1));;
+		setDebitInst(random);
+		return debitInst;
+	}
+
+	public int getDebitMoy() {
+		return debitMoy;
+	}
+
+	public void setDebitMoy(int debitMoy) {
+		this.debitMoy = debitMoy;
+	}
 
 }
