1cc2c2dd593e3ed6084049f601fdd257b870004c
adelaferte <apollinedelaferte@gmail.com>
2017-02-14 11:30:22 +0100

    initialisation des liens

--- Link.java
+++ Link.java
@@ -1,4 +1,71 @@
-
 public class Link {
+	
+	private Device A;
+	private Device B;
+	private int debitMin;
+	private int debitMax;
+	private int debitInst;
+	
+	/**
+	 * Constructeur
+	 * 
+	 * @param d1 initialise le device A
+	 * @param d2 initialise le device B
+	 * 
+	 * initialise les débits
+	 */
+	Link(Device d1, Device d2){
+		A = d1;
+		B = d2;
+		debitMax = 20;
+		debitMin = 10;
+		debitInst = 0;
+	}
+
+	public Device getA() {
+		return A;
+	}
+
+	public void setA(Device a) {
+		A = a;
+	}
+
+	public Device getB() {
+		return B;
+	}
+
+	public void setB(Device b) {
+		B = b;
+	}
+
+	public int getDebitMin() {
+		return debitMin;
+	}
+
+	public void setDebitMin(int debitMin) {
+		this.debitMin = debitMin;
+	}
+
+	public int getDebitMax() {
+		return debitMax;
+	}
+
+	public void setDebitMax(int debitMax) {
+		this.debitMax = debitMax;
+	}
+
+	public int getDebitInst() {
+		return debitInst;
+	}
+
+	public void setDebitInst(int debitInst) {
+		this.debitInst = debitInst;
+	}
+	
+	
+	
+	
+	
+	
 
 }
