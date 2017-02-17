5c1ed8dac0ae018d35e3d44de726615917f1cbea
Jimmy Rogala <jrogala@MacBook-Air-de-Jimmy.local>
2017-02-17 16:24:48 +0100

    another correction

--- Network.java
+++ Network.java
@@ -1,36 +1,45 @@
 public class Network {
-		public int size = 0;
-		public int debmin = 0;
-		public int debmax = 100;
-		public Network(int n){
-			size = n;
-			Device[] grid = new Device[n][n];
-			int num_link = 0;
-			for (i=0;i<n;i = i+2){
-				for (j=0;j<n;j = j+2){
-					if (i+1 < n){
-						num_link = new Link(grid[i][j],grid[i+1][j],debmin,debmax);
-						grid[i][j].addLink(num_link);
-						grid[i+1][j].addLink(num_link);
-					}
-					if (i-1 >=0){
-						num_link = new Link(grid[i][j],grid[i-1][j],debmin,debmax);
-						grid[i][j].addLink(num_link);
-						grid[i-1][j].addLink(num_link);
-					}
-					if (j+1 < n){
-						num_link = new Link(grid[i][j],grid[i][j+1],debmin,debmax);
-						grid[i][j].addLink(num_link);
-						grid[i][j+1].addLink(num_link);
-					}
-					if (j-1 >= 0){
-						num_link = new Link(grid[i][j],grid[i][j-1],debmin,debmax);
-						grid[i][j].addLink(num_link);
-						grid[i][j-1].addLink(num_link);
-					}
-				}
-			}
-			System.out.println("Network created.");
-		}
+    public int size = 0;
+    public int debmin = 0;
+    public int debmax = 100;
+    Link num_link;
+    public Device[][] grille;
+    public Network(int n){
+        size = n;
+        this.grille = new Device[size][size];
+        for (int i=0;i<size;i++){
+                for (int j=0;j<size;j++){
+                    this.grille[i][j] = new Device();
+                }
+        }
+        for (int i=0;i<size;i+=2){
+                for (int j=0;j<size;j+=2){
+                        if (is_valid(i+1,j,size) & is_valid(i,j,size)){
+                            num_link = new Link(this.grille[i][j],this.grille[i+1][j],debmin,debmax);
+                            this.grille[i][j].addLink(num_link);
+                            this.grille[i+1][j].addLink(num_link);
+                        }
+                        if (is_valid(i-1,j,size) & is_valid(i,j,size)){
+                            num_link = new Link(this.grille[i][j],this.grille[i-1][j],debmin,debmax);
+                            this.grille[i][j].addLink(num_link);
+                            this.grille[i-1][j].addLink(num_link);
+                        }
+                        if (is_valid(i,j+1,size) & is_valid(i,j,size)){
+                            num_link = new Link(this.grille[i][j],this.grille[i][j+1],debmin,debmax);
+                            this.grille[i][j].addLink(num_link);
+                            this.grille[i][j+1].addLink(num_link);
+                        }
+                        if (is_valid(i,j-1,size) & is_valid(i,j,size)){
+                            num_link = new Link(this.grille[i][j],this.grille[i][j-1],debmin,debmax);
+                            this.grille[i][j].addLink(num_link);
+                            this.grille[i][j-1].addLink(num_link);
+                        }
+                }
+            }
+            System.out.println("Network created.");
+        }
 
+    private boolean is_valid(int i, int j, int size) {
+        return(i>=0 & j >= 0 & i < size & j < size);
+    }
 }
