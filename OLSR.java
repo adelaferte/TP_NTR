d797dd5012ded5178d28b0062a8add5d92a26c84
adelaferte <apollinedelaferte@gmail.com>
2017-02-28 12:16:44 +0100

    OLSR qui marche pas

--- OLSR.java
+++ OLSR.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.Random;
 import java.util.ArrayList;
 
@@ -8,43 +9,125 @@ import java.util.ArrayList;
  */
 
 /**
- *
+ * 
  * @author jrogala
  */
 public class OLSR {
-    
-    int m = 200;
-    int i;
-    Device source;
-    Device destination;
-    int[] path;
-    
-    public OLSR(Device source, Device destination) {
-        this.source = source;
-        this.destination = destination;
-                
-    }
-    public int new_simulation() {
-        m = 20000;
-        Random r = new Random();
-        Device dencours = source;
-        while (dencours != destination){
-            Link lencours = dencours.getLinks().get(path[i]);
-            m = Math.min(m,lencours.getDebitInst());
-            dencours = lencours.getVoisin(dencours);
-            i++;
-        }
-    return m;
-    }
-    
-    private int parcours(Device d){
-        
-        private int[] aux(Device d){
-            
-            ArrayList<Link> links = d.getLinks();
-        
-        }
-        
-        
-    }
+
+	int m = 200;
+	int i;
+	Device source;
+	Device destination;
+	int[] path;
+
+	public OLSR(Network network) {
+		this.source = network.getFirst();
+		this.destination = network.getLast();
+		this.path = null;
+	}
+
+	public int new_simulation() {
+		m = 20000;
+		Random r = new Random();
+		Device dencours = source;
+		while (dencours != destination) {
+			Link lencours = dencours.getLinks().get(path[i]);
+			m = Math.min(m, lencours.getDebitInst());
+			dencours = lencours.getVoisin(dencours);
+			i++;
+		}
+		return m;
+	}
+
+	/*
+	 * private int parcours(Device d){
+	 * 
+	 * private int[] aux(Device d){
+	 * 
+	 * ArrayList<Link> links = d.getLinks();
+	 * 
+	 * }
+	 * 
+	 * 
+	 * }
+	 */
+
+	public ArrayList<Device> OLSR_1(int debitMin) {
+
+		int debitInt = 0;
+		int deviceHD;
+		ArrayList<Device> dejaVisite = new ArrayList<Device>();
+		dejaVisite.add(source);
+
+		// creation et initialisation d'un device "pointeur"
+		Device deviceEnCours = source;
+
+		// initialisation des liens du deviceEnCours avec ceux de la sources
+		ArrayList<Link> LinkEnCours = source.getLinks();
+
+		while (deviceEnCours != destination) {
+			// test valeur des débits de chaque lien
+			debitInt = LinkEnCours.get(0).getDebitMoy();
+			deviceHD = 0;
+			for (int j = 1; j < LinkEnCours.size(); j++) {
+				if (debitInt < LinkEnCours.get(j).getDebitMoy()) {
+					if (!dejaVisite.contains(LinkEnCours.get(j).getVoisin(
+							deviceEnCours))) {
+						deviceHD = j;
+						debitInt = LinkEnCours.get(0).getDebitMoy();
+					}
+				}
+			}
+			deviceEnCours = LinkEnCours.get(deviceHD).getVoisin(deviceEnCours);
+			dejaVisite.add(deviceEnCours);
+			if (debitMin > debitInt)
+				debitMin = debitInt;
+		}
+		return dejaVisite;
+	}
+
+	/**
+	 * GETTERS and SETTERS
+	 */
+
+	public int getM() {
+		return m;
+	}
+
+	public void setM(int m) {
+		this.m = m;
+	}
+
+	public int getI() {
+		return i;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
+
+	public Device getSource() {
+		return source;
+	}
+
+	public void setSource(Device source) {
+		this.source = source;
+	}
+
+	public Device getDestination() {
+		return destination;
+	}
+
+	public void setDestination(Device destination) {
+		this.destination = destination;
+	}
+
+	public int[] getPath() {
+		return path;
+	}
+
+	public void setPath(int[] path) {
+		this.path = path;
+	}
+
 }
