723665192efd4ef101a2c0ce24e67dcaa2621aff
jgachelin <contact@jgachelin.fr>
2017-03-07 11:05:35 +0100

    Amélioration toString()

--- Main.java
+++ Main.java
@@ -4,32 +4,31 @@ public class Main {
 	public static void main(String[] args) {
 		Network n = new Network(3);
 
-		System.out
-				.println("/***********************************OLSR*********************************/");
+		System.out.println("/*********************************OLSR*******************************/");
 
 		System.out.println("Network");
-		System.out.println(n.toStringDebMoy());
+		n.toStringDebMoy();
 
 		System.out.println("Parcours");
 		OLSR olsr = new OLSR(n);
 		ArrayList<Device> deviceVisiteolsr = olsr.OLSR_1();
 
-		System.out.println("liens visit�");
+		System.out.println("liens visite");
 		for (int i = 0; i < deviceVisiteolsr.size(); i++) {
 			System.out.println(deviceVisiteolsr.get(i).toStringDebMoy());
 		}
 
-		System.out
-				.println("\n\n\n\n/***********************************SLOR*********************************/");
+		System.out.println("\n\n\n\n");
+		System.out.println("/*********************************SLOR*******************************/");
 
 		System.out.println("Network");
-		System.out.println(n.toStringDebInst());
+		n.toStringDebInst();
 
 		System.out.println("Parcours");
 		LSOR lsor = new LSOR(n);
 		ArrayList<Device> deviceVisitelsor = lsor.LSOR_1();
 
-		System.out.println("liens visit�");
+		System.out.println("liens visite");
 		for (int i = 0; i < deviceVisitelsor.size(); i++) {
 			System.out.println(deviceVisitelsor.get(i).toStringDebInst());
 		}
