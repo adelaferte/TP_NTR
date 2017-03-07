9e0e19e0aed5c20941842a5cc8bdcd39b4d355aa
Jeremie Gachelin <17000416@e105m03.istic.univ-rennes1.fr>
2017-03-07 11:22:03 +0100

    Link

--- Link.java
+++ Link.java
@@ -87,10 +87,10 @@ public class Link {
 	}
 
 	public void toStringDebInst(){
-		System.out.println("[Debit instant "+getDebitInst()+" ]");
+		System.out.println("[Debit instant "+getDebitInst()+"] ");
 	}
 	
 	public void toStringDebMoy(){
-		System.out.println("[Debit moyen "+getDebitMoy()+" ]");
+		System.out.println("[Debit moyen "+getDebitMoy()+"] ");
 	}
 }
