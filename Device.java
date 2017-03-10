475f54d9c9d8c20aadba208537495534b8b0286d
Jimmy Rogala <jimmy.rogala@ens-rennes.fr>
2017-03-10 16:36:20 +0100

    added getlinkbetweennodes

--- Device.java
+++ Device.java
@@ -5,31 +5,41 @@ public class Device {
         public int i;
         public int j;
 	private ArrayList<Link> links;
-	
+
 	public Device() {
 		links = new ArrayList<Link>();
 	}
-	
+
 	public void addLink(Link dev) {
 		links.add(dev);
 	}
-	
+
 	public ArrayList<Link> getLinks() {
 		return this.links;
 	}
-	
+
 	public void toStringDebInst (){
 		for (int x = 0; x < links.size() ; x ++){
 			links.get(x).toStringDebInst();
 		}
 		System.out.println();
 	}
-	
+
 	public void toStringDebMoy (){
 		for (int x = 0; x < links.size() ; x ++){
 			links.get(x).toStringDebMoy();
 		}
 		System.out.println();
 	}
-	
+  public Link getLinksBetweenNodes(Device b){
+      for (int x = 0; x<links.size();x++) {
+          Device encours = b.getLinks().get(x).getVoisin(b);
+          if (encours == b) {
+              return b.getLinks().get(x);
+          }
+
+      }
+      return null;
+  }
+
 }
