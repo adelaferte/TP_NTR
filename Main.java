32acf383749358322a29e67876d9647915b54278
jgachelin <contact@jgachelin.fr>
2017-03-14 12:01:11 +0100

    Préparation du terrain pour l'arrivée des algos de Jimmy (qui se traine un peu)

--- Main.java
+++ Main.java
@@ -7,17 +7,21 @@ public class Main {
 		int tailleNetwork = 3;
 		Network n = createNetwork(tailleNetwork);
 		
-		while(true) {
+		boolean continu = true;
+		
+		while(continu) {
 			
 			System.out.println(" ___________________________________________________");
 			System.out.println(" |                                                  |");
 			System.out.println(" | Bienvenue !                                      |");
 			System.out.println(" | Choisissez votre algorithme de prédilection :    |");
-			System.out.println(" | ___ 1/ OLSR pas à pas                            |");
-			System.out.println(" | ___ 2/ SLOR pas à pas                            |");
-			System.out.println(" | ___ 3/ OLSR infinite                             |");
-			System.out.println(" | ___ 4/ Reset network                             |");
-			System.out.println(" | ___ 5/ Changer taille réseau                     |");
+			System.out.println(" | ___  1/ OLSR pas à pas                           |");
+			System.out.println(" | ___  2/ LSOR pas à pas                           |");
+			System.out.println(" | ___  3/ OLSR k                                   |");
+			System.out.println(" | ___  4/ LSOR k                                   |");
+			System.out.println(" | ___ 11/ Reset network                            |");
+			System.out.println(" | ___ 12/ Changer taille réseau                    |");
+			System.out.println(" | ___ 13/ Exit                                     |");
 			System.out.println(" |__________________________________________________|");
 			
 			System.out.print("\nVotre choix (1, 2, 3...) : ");
@@ -32,14 +36,30 @@ public class Main {
 				break;
 			case 2:main_slor(n, debitMinOlsr, deviceVisiteolsr);
 				break;
-			case 3:main_olsrInfinite(n, debitMinOlsr, deviceVisiteolsr);
+			case 3: System.out.print("Taille de k ? ");
+				int k = sc.nextInt();
+				main_olsrK(n, debitMinOlsr, deviceVisiteolsr, k);
 				break;
-			case 4:createNetwork(tailleNetwork);
+			case 4:
+				System.out.print("Taille de k ? ");
+				k = sc.nextInt();
+				main_lsorK(n, debitMinOlsr, deviceVisiteolsr, k);
 				break;
-			case 5:
-				System.out.print("Nouvelle taille ? ");
-				tailleNetwork = sc.nextInt();
+			case 11:createNetwork(tailleNetwork);
+				break;
+			case 12:
+				do {
+					System.out.print("Nouvelle taille ? ");
+					tailleNetwork = sc.nextInt();
+					if(tailleNetwork <= 0 || tailleNetwork > 10) {
+						System.out.println("/!\\ Erreur : taille max -> 10");
+					}
+				}while(tailleNetwork <= 0 || tailleNetwork > 10);
 				n = createNetwork(tailleNetwork);
+				break;
+			case 13:
+				continu = false;
+				System.out.println("Merci et à bientôt :-)");
 				break;
 			default:;
 			}
@@ -90,24 +110,32 @@ public class Main {
 		System.out.println("Debit Mini du parcours : " + debitMinLSOR);
 	}
 	
-	private static void main_olsrInfinite(Network n, int debitMinOlsr, ArrayList<Device> deviceVisiteolsr) {
-		System.out.println("/*********************************OLSRinfinite*******************************/");
-
-		System.out.println("Network");
-		n.toStringDeb();
-		
-		System.out.println("Parcours");
-		DelaiRef olsrinfinite = new DelaiRef(n);
-		debitMinOlsr = olsrinfinite.new_simulation();
-		
-		System.out.println("liens visite");
-		for (int i = 0; i < deviceVisiteolsr.size(); i++) {
-			deviceVisiteolsr.get(i).toStringDeb();
-		}
-		
-		System.out.println("Debit Mini du parcours : " + debitMinOlsr);
-		
-		System.out.println("\n\n\n\n");
+//	private static void main_olsrInfinite(Network n, int debitMinOlsr, ArrayList<Device> deviceVisiteolsr) {
+//		System.out.println("/*********************************OLSRinfinite*******************************/");
+//
+//		System.out.println("Network");
+//		n.toStringDeb();
+//		
+//		System.out.println("Parcours");
+//		DelaiRef olsrinfinite = new DelaiRef(n);
+//		debitMinOlsr = olsrinfinite.new_simulation();
+//		
+//		System.out.println("liens visite");
+//		for (int i = 0; i < deviceVisiteolsr.size(); i++) {
+//			deviceVisiteolsr.get(i).toStringDeb();
+//		}
+//		
+//		System.out.println("Debit Mini du parcours : " + debitMinOlsr);
+//		
+//		System.out.println("\n\n\n\n");
+//	}
+	
+	private static void main_olsrK(Network n, int debitMinOlsr, ArrayList<Device> deviceVisiteolsr, int k) {
+		System.out.println("/********* OLSR K : NOT YET IMPLEMENTED ********/");
+	}
+	
+	private static void main_lsorK(Network n, int debitMinOlsr, ArrayList<Device> deviceVisiteolsr, int k) {
+		System.out.println("/********* LSOR K : NOT YET IMPLEMENTED ********/");
 	}
 	
 }
