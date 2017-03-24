47b58466b5b7bad0e4b1549d4d657e03e1efb56f
Jimmy Rogala <jrogala@dy175008.univ-rennes1.fr>
2017-03-24 18:19:19 +0100

    nearly the end

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-            Simulation.main();
+            Simulation s = new Simulation();
+            s.Simulation(6, 7);
         }
 }
