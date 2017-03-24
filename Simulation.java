46c137719d2c6888340af6c6b04f2ee18ee25798
Jimmy Rogala <jimmy.rogala@ens-rennes.fr>
2017-03-24 18:19:19 +0100

    nearly the end

--- Simulation.java
+++ Simulation.java
@@ -1,38 +1,66 @@
 public class Simulation {
 	
-	
-	public static void main() {
+	public int[][][] waitList;
+	public void Simulation(int k,int tailleNetwork) {
 		// TO DO : Ajouter constructeur dans link, avec paramètre pour choisir l'écart de débit (90 à 110, 80 à 120, 50 à 150...)
 		// Dans Link : Surcharger le constructeur
 		
 		// Paramètres
-		int k = 0;
-		int tailleNetwork = 10;
+                
+                waitList = new int[tailleNetwork][tailleNetwork][k];
+
 		Network n = new Network(tailleNetwork);
-		LSORk Routage = new LSORk(n,k);
-		
+		LSORk[] Routages = new LSORk[k];
+                
+                for (int i = 0;i<k;i++){
+                    Routages[i] = new LSORk(n,i);
+                }
 		int TimeUnit = 0 ;
 		int delayRefreshValInst = 10;
 		
-		int dataToSend = 1000000;
-		Device Depart = n.getFirst();
+		int dataToSend = 100000;
+                for (int z = 0;z<k;z++){
+                    waitList[0][0][z] = dataToSend;
+                }
 		Device Arrive = n.getLast();
 		
-		Depart.waitlist = dataToSend;
-		Depart.futurwaitlist = dataToSend;
-		while(Arrive.waitlist != dataToSend) {
-                    if (TimeUnit%100 == 0 ){ // mets a jours les valeurs instantanees de chaque liens
-                            System.out.print((float)Arrive.waitlist/dataToSend*100 +"%\n");
+                int numberoffinish = 0;
+		while(numberoffinish < k) {
+                    if (TimeUnit%1000 == 0 ){ // mets a jours les valeurs instantanes de chaque liens
+                            System.out.println();
                     }
                     if (TimeUnit%delayRefreshValInst == 0 ){ // mets a jours les valeurs instantanes de chaque liens
                             n.reset();
                     }
-                    n.SendData(Routage);
-                    n.maj();
-                    TimeUnit++;// incremente l'unite de temps
+                    for (int i=0;i<k;i++){
+                        n.set(waitList,i);
+                        n.SendData(Routages[i]);
+                        n.maj();
+                        majwaitlist(n,i);
+                        TimeUnit++;// incremente l'unite de temps
+                    }
+                    for (int i = 0;i<k;i++){
+                        if(waitList[tailleNetwork-1][tailleNetwork -1][i] == dataToSend){
+                            System.out.println("LSOR "+ i + " finished in "+ TimeUnit + " time Unit");
+                            waitList[tailleNetwork-1][tailleNetwork -1][i] = 0;
+                            numberoffinish++;
+                        }
+                    }
 		}
 		
-		
-		System.out.println("Le delay d'envoi de "+dataToSend+" data est de :"+TimeUnit );
 	}
+        public void majwaitlist(Network n,int i){
+            for ( int x  = 0 ; x < n.size ; x ++ ){
+    		for (int y = 0 ; y < n.size ; y ++ ){
+    			waitList[x][y][i] = n.devicegrille[x][y].waitlist;
+    		}
+            }
+        }
+        public boolean isFinish(int val,int tailleNetwork){
+            boolean flag = false;
+            for (int i = 0;i<waitList[0][0].length;i++){
+                flag = flag && (waitList[tailleNetwork-1][tailleNetwork-1][i] == -1);
+            }
+            return flag;
+        }
 }
